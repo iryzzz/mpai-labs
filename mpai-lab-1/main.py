7ea85a17f1f8b64ba7b896a8e67c4058269d19aa
iryzzz <58042184+iryzzz@users.noreply.github.com>
2022-02-16 22:47:49 +0400

    Update main.py

--- mpai-lab-1/main.py
+++ mpai-lab-1/main.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import data, img_as_float, exposure
+from skimage.io import imread, imsave, imshow, show
 
 
 
@@ -21,4 +22,33 @@ def get_user_parameters():
     return parameters
 
 
-print (get_user_parameters())
+def plot_img_and_hist(image, axes, bins=256):
+    """Plot an image along with its histogram and cumulative histogram.
+
+    """
+    image = img_as_float(image)
+    ax_img, ax_hist = axes
+    ax_cdf = ax_hist.twinx()
+
+    # Display image
+    ax_img.imshow(image, cmap=plt.cm.gray)
+    ax_img.set_axis_off()
+
+    # Display histogram
+    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
+    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax_hist.set_xlabel('Pixel intensity')
+    ax_hist.set_xlim(0, 1)
+    ax_hist.set_yticks([])
+
+    # Display cumulative distribution
+    img_cdf, bins = exposure.cumulative_distribution(image, bins)
+    ax_cdf.plot(bins, img_cdf, 'r')
+    ax_cdf.set_yticks([])
+
+    return ax_img, ax_hist, ax_cdf
+
+params = get_user_parameters()
+input_path = params.get('path_in')
+source_img = imread(input_path)
+
